0cc14765a37fd43139ddb2b2b6538bba55f8db3b
yuseungrii <siso1210@gmail.com>
2021-11-08 23:22:45 +0900

    commit1

--- todo-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-/* index.html의 root div를 갖고온다 */
-ReactDOM.render(<App />, document.getElementById("root"));
